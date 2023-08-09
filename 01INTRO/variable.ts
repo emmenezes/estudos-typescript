b78b529e4cd0ddf0f66e89864957dbbd6a87630b
Emme <menezes.ecd@gmail.com>
2023-08-09 17:57:16 -0300

    Cria variable.ts

---
+++ 01INTRO/variable.ts
@@ -0,0 +1,21 @@
+let greetings: string = "OMG hiii";
+
+// number
+let userId: number = 123123;
+
+// boolean
+let isLoggedIn: boolean = false;
+
+// Avoid explain the obvious
+
+let hero;
+
+function getHero() {
+  return true;
+}
+
+hero = getHero();
+
+// Avoid type 'any'
+
+console.log(greetings);
